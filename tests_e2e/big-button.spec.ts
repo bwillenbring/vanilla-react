dc17a1b4ffa35f7b3444c9f4cb4074634d7cfe9f
Ben Willenbring <ben@onebrief.com>
2024-07-26 11:12:37 -0400

    Update big-button.spec.ts
    Add a few more e2e tests

--- tests_e2e/big-button.spec.ts
+++ tests_e2e/big-button.spec.ts
@@ -1,41 +1,81 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('/');
+  await page.goto("/");
 });
 
-test('clicks on shuffler', async ({ page }) => {
-  // Set up flags 
-  let dialogCalled = false;
-  let dialogDismissed = false;
-  let dialogMessage = '';
+test("clicks on button that displays shuffled array", async ({ page }) => {
+  // Dismiss the alert as soon as it appears
+  page.on("dialog", (dialog) => dialog.dismiss());
 
-  // Set up a listener for the alert function 
-  page.on('dialog', async dialog => {
-    // Toggle the dialogCalled flag
-    dialogCalled = true;
-    dialogMessage = dialog.message();
-    await dialog.dismiss();
-    dialogDismissed = true;
-  });
-  
-  // Get the button
-  const button = page.getByTestId('big-button-shuffler')
-  await expect(button).toBeVisible();
+  // Set up a waitForEvent on an alert
+  const dialogPromise = page.waitForEvent("dialog");
+
+  // Get the button and click it and await the dialog
+  const button = page.getByTestId("big-button-shuffler");
   await button.click();
-  // Assertions 
-  expect(dialogCalled).toBe(true);
-  expect(dialogDismissed).toBe(true);
-  expect(dialogMessage).toContain('Larry');
+  const dialog = await dialogPromise;
+
+  // Assert the dialog type is alert and the message is one of the days
+  expect(dialog.type()).toBe("alert");
+  // For each item in expectedArray, expect it to be in the dialog message
+  for (const item of ["Larry", "Curly", "Moe"]) {
+    expect(dialog.message()).toContain(item);
+  }
 });
 
-test("clicks on console.log", async ({ page }) => {
-  const button = page.locator('[data-testid*="console-log"]');
-  // Assert that the word "foo" is in the console log
-  await button.click();
-  // Get the contents of the console.log() and assert that it contains 'Katie'
-  const consoleLog = await page.$eval("text=foo", (el) => el.textContent);
+test("clicks on button that alerts the current day", async ({ page }) => {
+  // Dismiss the alert as soon as it appears
+  page.on("dialog", (dialog) => dialog.dismiss());
+  // Set up days of week (the displayed message will be one of these days)
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
-  // const consoleLog = await page.$eval("text=foo", (el) => el.textContent);
-  // expect(consoleLog).toContain("Katie poured coffee on father's grey suit");
+  // Set up a waitForEvent on an alert
+  const dialogPromise = page.waitForEvent("dialog");
+
+  // Get the button and click it and await the dialog
+  const button = page.getByTestId("big-button-get-current-day");
+  await button.click();
+  const dialog = await dialogPromise;
+
+  // Assert the dialog type is alert and the message is one of the days
+  expect(dialog.type()).toBe("alert");
+  expect(days).toContain(dialog.message());
+});
+
+test("clicks on button that triggers an error", async ({ page }) => {
+  // Get the button that will trigger the console.log
+  const button = page.getByTestId("big-button-throw-error");
+  const consolePromise = page.waitForEvent("pageerror");
+
+  // Click the  button which triggers the console.log and trap the message
+  await button.click();
+  const msg = await consolePromise;
+
+  // Assert the console message matches expectation
+  expect(msg).not.toBeNull();
+  expect(msg.message).toMatch(/error/gim);
+});
+
+test("clicks on button that triggers console.log", async ({ page }) => {
+  // Get the button that will trigger the console.log
+  const button = page.getByTestId("big-button-console-log");
+  const consolePromise = page.waitForEvent("console");
+
+  // Click the  button which triggers the console.log
+  await button.click();
+
+  // Trap the console.log message
+  const msg = await consolePromise;
+
+  // Assert the console message matches expectation
+  expect(msg.text()).toContain("Katie");
 });
