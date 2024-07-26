27317576720843e453aceda14d7a814894d03675
Ben Willenbring <ben@onebrief.com>
2024-07-26 14:59:46 -0400

    Update App.test.tsx
    Formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import {App} from "./App";
+import { render, screen } from "@testing-library/react";
+import { App } from "./App";
 
 describe("App component rendering", () => {
   test("renders App", () => {
