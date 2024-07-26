3be96d20ff18bb8cfd528da0ddaec09271c4783c
Ben Willenbring <ben@onebrief.com>
2024-07-26 10:25:34 -0400

    feat: add accessibility checking

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
 module.exports = {
+  setupFiles: ["./jest.setup.js"],
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  transformIgnorePatterns: [
-    "/node_modules/",
-    "<rootDir>/tests_e2e/"
-  ],
+  transformIgnorePatterns: ["/node_modules/", "<rootDir>/tests_e2e/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   testMatch: ["**/src/**/*.test.(ts|tsx)"],
 };
