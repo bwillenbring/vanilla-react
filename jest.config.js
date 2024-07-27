35bfc17dd80fc77653c01b5afd111ed8722e471e
Ben Willenbring <ben@onebrief.com>
2024-07-27 18:46:21 -0400

    feat: add sonarjs

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,5 @@ module.exports = {
   transformIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests_e2e/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  testMatch: ["**/src/**/*.test.(ts|tsx)"],
+  testMatch: ["**/src/**/*.test.(ts|tsx)", "**/bin/**/*.test.(ts|tsx)"],
 };
